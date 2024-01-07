76df75dbf0531a5e13448af24f6bf37572b109e1
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-07 12:20:25 +0200

    feat(CW-2) create sidebar searchbar js and css files

---
+++ src/components/SideBar/_parts/components/SearchBar/SearchBar.js
@@ -0,0 +1,10 @@
+import './SearchBar.css';
+
+
+export default function SearchBar() {
+    return (
+        <div className='sidebar-searchbar-container'>
+            search bar
+        </div>
+    )
+}
