0f4b41e08876abce888303b985725e40679eba88
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-07 11:51:57 +0200

    feat(CW-1) create message bar js and css files

--- src/components/ChatWindow/ChatWindow.js
+++ src/components/ChatWindow/ChatWindow.js
@@ -4,13 +4,14 @@ import Grid from '@mui/material/Grid';
 import './ChatWindow.css';
 import Header from './_parts/components/Header/Header';
 import ChatBox from './_parts/components/ChatBox/ChatBox';
+import MessageBar from './_parts/components/MessageBar/MessageBar';
 
 export default function ChatWindow() {
     return (
         <div className='chat-window-container'>
             <Header/>
             <ChatBox/>
-
+            <MessageBar/>
         </div>
     )
 }
---
+++ src/components/ChatWindow/_parts/components/MessageBar/MessageBar.js
@@ -0,0 +1,12 @@
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import './MessageBar.css';
+
+export default function MessageBar() {
+  return (
+    <div className='chat-message-bar-container'>chat messsage bar
+
+    </div>
+  )
+}
