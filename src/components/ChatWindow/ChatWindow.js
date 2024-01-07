60ed9afefe852cea4347fb1ee315c4b70d919513
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-07 11:46:37 +0200

    feat(CW-1) create chat box js and css files

--- src/components/ChatWindow/ChatWindow.js
+++ src/components/ChatWindow/ChatWindow.js
@@ -3,11 +3,13 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import './ChatWindow.css';
 import Header from './_parts/components/Header/Header';
+import ChatBox from './_parts/components/ChatBox/ChatBox';
 
 export default function ChatWindow() {
     return (
         <div className='chat-window-container'>
             <Header/>
+            <ChatBox/>
 
         </div>
     )
