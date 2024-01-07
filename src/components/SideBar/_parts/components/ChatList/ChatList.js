775e4200c63d09df268e4f2b8dcb8889e3d26646
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-07 12:20:36 +0200

    feat(CW-2) create sidebar chat list js and css files

---
+++ src/components/SideBar/_parts/components/ChatList/ChatList.js
@@ -0,0 +1,10 @@
+import './ChatList.css';
+
+
+export default function ChatList() {
+    return (
+        <div className='sidebar-chat-list-container'>
+            chat list
+        </div>
+    )
+}
