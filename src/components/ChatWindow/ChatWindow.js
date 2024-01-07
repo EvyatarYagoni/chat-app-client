11987148e984642883368f9e5408f5dd6114b3e2
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-07 11:44:38 +0200

    feat(CW-1) create header js and css files

--- src/components/ChatWindow/ChatWindow.js
+++ src/components/ChatWindow/ChatWindow.js
@@ -2,10 +2,13 @@ import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import './ChatWindow.css';
+import Header from './_parts/components/Header/Header';
 
 export default function ChatWindow() {
     return (
-        <div className='chat-window-container'>chat window
+        <div className='chat-window-container'>
+            <Header/>
+
         </div>
     )
 }
---
+++ src/components/ChatWindow/_parts/components/Header/Header.js
@@ -0,0 +1,12 @@
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className='chat-header-container'>chat header
+
+    </div>
+  )
+}
