ece71beb3d6173edb03356d0d2949391be8ec5b5
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-08 16:12:57 +0200

    feat(CW-Login) add <StrictMode> for highlight problems earlier

--- src/App.js
+++ src/App.js
@@ -2,35 +2,38 @@ import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import {publicRoutes, protectedRoutes} from "./routes/index";
 import {Helmet} from "react-helmet";
 import ProtectedRoutes from "./components/ProtectedRoutes/ProtectedRoutes";
+import {StrictMode} from "react";
 
 
 function App() {
   return (
-    <BrowserRouter>
-        <Helmet>
-            <link
-                rel="stylesheet"
-                href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-            />
-            <link
-                rel="stylesheet"
-                href="https://fonts.googleapis.com/icon?family=Material+Icons"
-            />
+    <StrictMode>
+      <BrowserRouter>
+          <Helmet>
+              <link
+                  rel="stylesheet"
+                  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+              />
+              <link
+                  rel="stylesheet"
+                  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+              />
 
-        </Helmet>
-        <Routes>
-          <Route element={<ProtectedRoutes isAllowed={false} />}>
-            {protectedRoutes.map((route, index) => (
-              <Route key={index} path={route.path} element={route.element}/>
-            ))}
-          </Route>
-          <Route>
-            {publicRoutes.map((route, index) => (
-              <Route key={index} path={route.path} element={route.element}/>
-            ))}
-          </Route>
-        </Routes>
-    </BrowserRouter>
+          </Helmet>
+          <Routes>
+            <Route element={<ProtectedRoutes isAllowed={false} />}>
+              {protectedRoutes.map((route, index) => (
+                <Route key={index} path={route.path} element={route.element}/>
+              ))}
+            </Route>
+            <Route>
+              {publicRoutes.map((route, index) => (
+                <Route key={index} path={route.path} element={route.element}/>
+              ))}
+            </Route>
+          </Routes>
+      </BrowserRouter>
+    </StrictMode>
   );
 }
 
