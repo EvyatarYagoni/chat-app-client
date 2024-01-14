62007659f4fc314ac546369bcb462e15d74725d1
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-14 14:24:20 +0200

    feat(CW-Login) update protected route logic

--- src/components/ProtectedRoutes/ProtectedRoutes.js
+++ src/components/ProtectedRoutes/ProtectedRoutes.js
@@ -1,15 +1,43 @@
-import {Navigate, Outlet} from "react-router-dom";
-import {useSelector} from "react-redux";
+import {Navigate, Outlet, useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect, useState} from "react";
+import axiosInstance  from "../../config/axios/axiosInstance";
+import {setAccessToken, setUser} from "../../store/slices/auth/authSlice";
 
 const ProtectedRoutes = () => {
-  console.log('ProtectedRoutes');
-  const user = useSelector(state => state.user.value);
-  console.log('user', user);
-  if (!user) {
-    return <Navigate to={'/login'} replace />;
+  const user = useSelector(state => state.auth.user);
+  const accessToken = useSelector(state => state.auth.accessToken);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [isAuthLoading, setIsAuthLoading] = useState(true);
+  const redirectToHomePage = () => {
+    navigate('/');
   }
 
-  return <Outlet />;
+  const refreshToken = async () => {
+    try {
+      // Try to create new access token with refresh token
+      const newAccessToken = await axiosInstance.get('/auth/refresh-token');
+      dispatch(setAccessToken(true));
+      dispatch(setUser({name: "evyatar"}));
+      redirectToHomePage();
+      setIsAuthLoading(false);
+    } catch (err) {
+      console.log(err);
+      navigate('/login');
+    }
+  }
+  useEffect(() => {
+    if (!user || !accessToken) {
+      refreshToken();
+    }
+  }, [user, accessToken]);
+
+  if (isAuthLoading) {
+    return <></>
+  }
+
+  return user && accessToken ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoutes;
