324345406af101751f5552d933f9a446676902e3
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-15 14:14:50 +0200

    feat(CW-Login) config axios with access token

--- src/config/axios/axiosInstance.js
+++ src/config/axios/axiosInstance.js
@@ -3,7 +3,17 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   withCredentials: true, // Enable sending credentials (cookies) with every request
-  baseURL: process.env.REACT_APP_SERVER_API_URL // Set your base URL
+  baseURL: process.env.REACT_APP_SERVER_API_URL, // Set your base URL
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+  }
 });
+
+// if accessToken is invalid send to /auth/refresh-token to get a new one and save it on localStorage.
+// if refreshToken is invalid send to /auth/logout to clear cookies and remove refreshToken from database and redirect to login page
+
+axiosInstance.interceptors.request.use(
+
+)
 
 export default axiosInstance;
