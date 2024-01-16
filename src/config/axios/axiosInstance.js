acf8438686567206617a43868ab80d70f89a387d
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-16 09:55:17 +0200

    feat(CW-Login) attach accessToken for each sub-request

--- src/config/axios/axiosInstance.js
+++ src/config/axios/axiosInstance.js
@@ -4,16 +4,25 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   withCredentials: true, // Enable sending credentials (cookies) with every request
   baseURL: process.env.REACT_APP_SERVER_API_URL, // Set your base URL
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem('accessToken')}`
-  }
 });
 
-// if accessToken is invalid send to /auth/refresh-token to get a new one and save it on localStorage.
-// if refreshToken is invalid send to /auth/logout to clear cookies and remove refreshToken from database and redirect to login page
 
+// Add a request interceptor
 axiosInstance.interceptors.request.use(
+  (config) => {
+    // Modify the request config before sending it
 
-)
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    // Handle request errors
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
