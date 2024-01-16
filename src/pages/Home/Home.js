c7a9392bbbed3fef1c97a43313911c7e4c9b8702
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-16 09:55:44 +0200

    feat(CW-Login) fetch user data

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,23 +5,35 @@ import axiosInstance from "../../config/axios/axiosInstance";
 import {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {setUser} from "../../store/slices/auth/authSlice";
+import {useNavigate} from "react-router-dom";
 
 export default function Home() {
+
   const user = useSelector(state => state.auth.user);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const fetchUser = async () => {
     try {
       const response = await axiosInstance.post('/auth/me');
       console.log(response.data);
       dispatch(setUser(response.data));
     } catch (err) {
-      console.log(err);
+      try {
+        const response = await axiosInstance.get('/auth/refresh-token');
+        const newAccessToken = response.data.accessToken;
+        localStorage.setItem('accessToken', newAccessToken);
+        const response2 = await axiosInstance.post('/auth/me');
+        return dispatch(setUser(response2.data));
+      } catch (err) {
+        console.log(err);
+        localStorage.removeItem('accessToken');
+        return navigate('/login');
+      }
     }
   }
 
   useEffect(() => {
-    console.log('USE EFFEFCT user', user);
-    fetchUser();
+      fetchUser();
   }, []);
 
 
