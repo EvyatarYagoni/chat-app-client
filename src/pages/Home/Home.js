99b2704c52d3c77ba7189c092616c965998c9852
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-16 15:45:34 +0200

    feat(CW-NCM) fetch all users for new conversation menu

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,7 +4,7 @@ import ChatWindow from "../../components/ChatWindow/ChatWindow";
 import axiosInstance from "../../config/axios/axiosInstance";
 import {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {setUser} from "../../store/slices/auth/authSlice";
+import {setUser, setAllUsers} from "../../store/slices/auth/authSlice";
 import {useNavigate} from "react-router-dom";
 
 export default function Home() {
@@ -12,18 +12,17 @@ export default function Home() {
   const user = useSelector(state => state.auth.user);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const fetchUser = async () => {
+  const fetchLoggedUser = async () => {
     try {
       const response = await axiosInstance.post('/auth/me');
-      console.log(response.data);
-      dispatch(setUser(response.data));
+      dispatch(setUser(response.data.data));
     } catch (err) {
       try {
         const response = await axiosInstance.get('/auth/refresh-token');
         const newAccessToken = response.data.accessToken;
         localStorage.setItem('accessToken', newAccessToken);
         const response2 = await axiosInstance.post('/auth/me');
-        return dispatch(setUser(response2.data));
+        return dispatch(setUser(response2.data.data));
       } catch (err) {
         console.log(err);
         localStorage.removeItem('accessToken');
@@ -32,8 +31,22 @@ export default function Home() {
     }
   }
 
+  const fetchAllUsers = async () => {
+    try {
+      const response = await axiosInstance.get('/users');
+      const allUsers = response.data.data;
+      // replace the username of logged user with 'You'
+      const loggedUser = allUsers.find(user => user.id === user.id);
+      loggedUser.username = 'You';
+      dispatch(setAllUsers(allUsers));
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   useEffect(() => {
-      fetchUser();
+    fetchLoggedUser();
+    fetchAllUsers();
   }, []);
 
 
