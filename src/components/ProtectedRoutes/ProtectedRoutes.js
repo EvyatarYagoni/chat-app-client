cfa7b0bc3140527b541557f7afcf5e7885bdc5b6
Evyatar Yagoni <evyatar@hackeruso.com>
2024-01-16 09:58:39 +0200

    feat(CW-Login) if no access token redirect to login page

--- src/components/ProtectedRoutes/ProtectedRoutes.js
+++ src/components/ProtectedRoutes/ProtectedRoutes.js
@@ -1,20 +1,8 @@
-import {Navigate, Outlet, useNavigate} from "react-router-dom";
-import {useSelector} from "react-redux";
-import {useEffect} from "react";
+import {Navigate, Outlet} from "react-router-dom";
 import {APP_ROUTES} from "../../utils/constants";
 
 const ProtectedRoutes = () => {
-  const user = useSelector(state => state.auth.user);
-  const navigate = useNavigate();
-
   const accessToken = localStorage.getItem('accessToken');
-
-  useEffect(() => {
-    if (!accessToken) {
-      return navigate(APP_ROUTES.LOGIN);
-    }
-  }, [user]);
-
   return accessToken ? <Outlet /> : <Navigate to={APP_ROUTES.LOGIN} />;
 };
 
